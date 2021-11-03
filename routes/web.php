493b3287310334c53c7e2bef7fda58ef6200dc9d
IDCOB230 <IDCOB230@IDCOB230>
2021-11-03 16:48:48 -0300

    Duas novas rotas: Categoria e categorias.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+Route::get('/categoria/{flag}/posts', function($flag){
+    return "Posts da categoria: {$flag}";
+});
+
+Route::get('/categorias/{flag}', function($prim1){
+    return "Categorias: {$prim1}";
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
