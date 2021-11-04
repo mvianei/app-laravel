d734b7d2c52433078ed7cbf1ca551c56798d98c1
IDCOB230 <IDCOB230@IDCOB230>
2021-11-04 16:42:17 -0300

    Novas rotas.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+Route::delete('products/{id}','ProductController@destroy')->name('products.destroy');
+Route::put('products/{id}/update','ProductController@update')->name('products.update');
+Route::get('products/{id}/edit','ProductController@edit')->name('products.edit');
+Route::get('products/create','ProductController@create')->name('products.create');
+Route::get('products/{id}/show','ProductController@show')->name('products.show');
+Route::get('products','ProductController@index')->name('products.index');
+
 Route::group([
     'middleware' => [],
     'prefix' => 'admin',
